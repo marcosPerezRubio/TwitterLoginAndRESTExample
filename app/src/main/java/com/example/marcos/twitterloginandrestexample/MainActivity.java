e23e3cc68625bb7c10d37e3d4530061cc1b380cc
marcos <marcos.perez@est.fib.upc.edu>
2017-02-01 15:47:12 +0100

    Fabric configured

--- app/src/main/java/com/example/marcos/twitterloginandrestexample/MainActivity.java
+++ app/src/main/java/com/example/marcos/twitterloginandrestexample/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.marcos.twitterloginandrestexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "8MYRlSOWhDBBfO1UGdXajIveh";
+    private static final String TWITTER_SECRET = "LP3mGbw1pgrcgk9hwJ2yGhf681vZTkGRosfWYkweLgJ1qclyHp";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 }
